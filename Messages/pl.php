234c849ce629976c82994284463686c55469efc8
Nikolay Beketov <nbek@miko.ru>
2021-03-26 10:32:34 +0000

    Translated using Weblate (Polish)
    Currently translated at 100.0% (13 of 13 strings)
    
    Translation: MIKOPBX/ModulePT1CCore
    Translate-URL: https://weblate.mikopbx.com/projects/mikopbx/modulept1ccore/pl/

--- Messages/pl.php
+++ Messages/pl.php
@@ -1,1 +1,23 @@
 <?php
+return [
+    'f2b_Jail_module_p_t1_c_core' => 'Błędy podczas autoryzacji i klientów CTI lub paneli 1C',
+    'fw_modulept1ccoreDescription' => 'CTI CLIENT - Panel telefoniczny 1.0 dla 1C',
+    'module_pt1c_core_LinksRussian' => 'Przydatne linki (rosyjski)',
+    'module_pt1c_core_Forum' => 'Forum',
+    'module_pt1c_core_WebinarLinkRussian' => 'Webinar na temat konfiguracji modułu',
+    'module_pt1c_core_WikiDocsRussian' => 'Dokumentacja konfiguracji modułu Wiki',
+    'module_pt1c_coreUpdateStatus' => 'Aktualizacja statusu',
+    'module_pt1c_coreDisconnected' => 'Moduł wyłączony',
+    'module_pt1c_coreConnected' => 'Moduł podłączony',
+    'SubHeaderModulePT1CCore' => 'Moduł kontroli połączeń PC',
+    'BreadcrumbModulePT1CCore' => 'Panel telefoniczny 1.0 dla 1C',
+    'mo_ModuleModulePT1CCore' => 'Panel telefoniczny 1.0 dla 1C',
+    /**
+ * Copyright (C) MIKO LLC - All Rights Reserved
+ * Unauthorized copying of this file, via any medium is strictly prohibited
+ * Proprietary and confidential
+ * Written by Nikolay Beketov, 6 2018
+ *
+ */
+    'repModulePT1CCore' => 'Panel telefoniczny 1.0 dla 1C: Enterprise -% repesent%',
+];

768f3edccce08adfd1ae30111292d69c3ad8609f
Nikolay Beketov <nbek@miko.ru>
2021-11-21 15:20:53 +0000

    Translated using Weblate (Swedish)
    Currently translated at 100.0% (13 of 13 strings)
    
    Translation: MIKOPBX/ModulePT1CCore
    Translate-URL: https://weblate.mikopbx.com/projects/mikopbx/modulept1ccore/sv/

--- Messages/sv.php
+++ Messages/sv.php
@@ -1,1 +1,23 @@
 <?php
+return [
+    /**
+ * Copyright (C) MIKO LLC - All Rights Reserved
+ * Unauthorized copying of this file, via any medium is strictly prohibited
+ * Proprietary and confidential
+ * Written by Nikolay Beketov, 6 2018
+ *
+ */
+    'repModulePT1CCore' => 'Telefonipanel 1.0 för 1C: Enterprise - % represent%',
+    'SubHeaderModulePT1CCore' => 'PC-anropskontrollmodul',
+    'mo_ModuleModulePT1CCore' => 'Telefonipanel 2.0 för 1C',
+    'BreadcrumbModulePT1CCore' => 'Telefonipanel 2.0 för 1C',
+    'module_pt1c_coreConnected' => 'Modul ansluten',
+    'module_pt1c_coreDisconnected' => 'Modul inaktiverad',
+    'module_pt1c_coreUpdateStatus' => 'Statusuppdatering',
+    'module_pt1c_core_WikiDocsRussian' => 'Wiki-dokumentation för att konfigurera modulen',
+    'module_pt1c_core_WebinarLinkRussian' => 'Webinarium för moduluppsättning',
+    'module_pt1c_core_Forum' => 'Forum',
+    'module_pt1c_core_LinksRussian' => 'Användbara länkar (ryska)',
+    'fw_modulept1ccoreDescription' => 'CTI CLIENT - Telefonipanel 2.0 för 1C',
+    'f2b_Jail_module_p_t1_c_core' => 'Fel under auktorisering och CTI-klienter eller 1C-paneler',
+];

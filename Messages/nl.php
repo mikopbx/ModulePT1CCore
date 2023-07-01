9b579f740b67795d8b951d1c33d97105341e3156
Jochem Pluim <jochem@cafedoranjeboom.nl>
2023-07-01 23:21:45 +0000

    Translated using Weblate (Dutch)
    Currently translated at 100.0% (13 of 13 strings)
    
    Translation: MIKOPBX/ModulePT1CCore
    Translate-URL: https://weblate.mikopbx.com/projects/mikopbx/modulept1ccore/nl/

--- Messages/nl.php
+++ Messages/nl.php
@@ -1,1 +1,23 @@
 <?php
+return [
+    'f2b_Jail_module_p_t1_c_core' => 'Fouten bij het autoriseren van CTI-clients of 1C-panels',
+    'fw_modulept1ccoreDescription' => 'CTI CLIENT - Telefoniepaneel 1.0 voor 1C',
+    'module_pt1c_core_LinksRussian' => 'Nuttige links (Russisch)',
+    'module_pt1c_core_Forum' => 'Forum',
+    'module_pt1c_core_WebinarLinkRussian' => 'Webinar over het opzetten van de module',
+    'module_pt1c_core_WikiDocsRussian' => 'Wiki-documentatie voor het instellen van de module',
+    'module_pt1c_coreUpdateStatus' => 'Status update',
+    'module_pt1c_coreDisconnected' => 'Module uitgeschakeld',
+    'module_pt1c_coreConnected' => 'module aangesloten',
+    'SubHeaderModulePT1CCore' => 'Module voor het beheren van oproepen vanaf een computer',
+    'BreadcrumbModulePT1CCore' => 'Telefoniepaneel 1.0 voor 1C',
+    'mo_ModuleModulePT1CCore' => 'Telefoniepaneel 1.0 voor 1C',
+    /**
+ * Copyright (C) MIKO LLC - All Rights Reserved
+ * Unauthorized copying of this file, via any medium is strictly prohibited
+ * Proprietary and confidential
+ * Written by Nikolay Beketov, 6 2018
+ *
+ */
+    'repModulePT1CCore' => 'Telefoniepaneel 1.0 voor 1C:Enterprise - %repesent%',
+];

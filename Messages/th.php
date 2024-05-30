1c9d5ab49e8fccf2492d4adbde9545bd252b8349
SUMONCHAI WONGPITAK <sumonchai@gmail.com>
2024-05-30 09:23:55 +0000

    Translated using Weblate (Thai)
    Currently translated at 69.2% (9 of 13 strings)
    
    Translation: MIKOPBX/ModulePT1CCore
    Translate-URL: https://weblate.mikopbx.com/projects/mikopbx/modulept1ccore/th/

--- Messages/th.php
+++ Messages/th.php
@@ -1,1 +1,12 @@
 <?php
+return [
+    'SubHeaderModulePT1CCore' => 'โมดูลสำหรับจัดการการโทรจากคอมพิวเตอร์',
+    'module_pt1c_coreConnected' => 'เชื่อมต่อโมดูลแล้ว',
+    'module_pt1c_coreDisconnected' => 'ปิดใช้งานโมดูลแล้ว',
+    'module_pt1c_coreUpdateStatus' => 'อัพเดทสถานะ',
+    'module_pt1c_core_WikiDocsRussian' => 'เอกสาร Wiki สำหรับการตั้งค่าโมดูล',
+    'module_pt1c_core_WebinarLinkRussian' => 'การสัมมนาผ่านเว็บเกี่ยวกับการตั้งค่าโมดูล',
+    'module_pt1c_core_Forum' => 'ฟอรั่ม',
+    'module_pt1c_core_LinksRussian' => 'ลิงค์ที่มีประโยชน์ (รัสเซีย)',
+    'fw_modulept1ccoreDescription' => 'CTI CLIENT - แผงโทรศัพท์ 1.0 สำหรับ 1C',
+];

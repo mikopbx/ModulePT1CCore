035f0fb3e8ce84a8741d07228d0ca9417e03e476
Juhász Béla <korfa@freemail.hu>
2024-07-30 12:41:33 +0000

    Translated using Weblate (Hungarian)
    Currently translated at 84.6% (11 of 13 strings)
    
    Translation: MIKOPBX/ModulePT1CCore
    Translate-URL: https://weblate.mikopbx.com/projects/mikopbx/modulept1ccore/hu/

--- Messages/hu.php
+++ Messages/hu.php
@@ -1,1 +1,14 @@
 <?php
+return [
+    'BreadcrumbModulePT1CCore' => 'Telefonpanel 1.0 1C-hez',
+    'SubHeaderModulePT1CCore' => 'Modul a számítógépes hívásvezérléshez',
+    'module_pt1c_coreConnected' => 'Modul csatlakoztatva',
+    'module_pt1c_coreDisconnected' => 'Modul letiltva',
+    'module_pt1c_coreUpdateStatus' => 'Állapotfrissítés',
+    'module_pt1c_core_WebinarLinkRussian' => 'Webinárium a modul beállításáról',
+    'module_pt1c_core_Forum' => 'Fórum',
+    'module_pt1c_core_LinksRussian' => 'Hasznos linkek (orosz)',
+    'f2b_Jail_module_p_t1_c_core' => 'Hibák a CTI-ügyfelek vagy 1C-panelek engedélyezése során',
+    'module_pt1c_core_WikiDocsRussian' => 'Wiki dokumentáció a modul konfigurálásáról',
+    'fw_modulept1ccoreDescription' => 'CTI CLIENT - Telefonos panel 1.0 az 1C számára',
+];

f4739336b2344dfda60a8a0a7a9356d41a98174d
Juhász Béla <korfa@freemail.hu>
2024-08-22 07:12:24 +0000

    Translated using Weblate (Hungarian)
    Currently translated at 100.0% (13 of 13 strings)
    
    Translation: MIKOPBX/ModulePT1CCore
    Translate-URL: https://weblate.mikopbx.com/projects/mikopbx/modulept1ccore/hu/

--- Messages/hu.php
+++ Messages/hu.php
@@ -1,14 +1,23 @@
 <?php
 return [
     'BreadcrumbModulePT1CCore' => 'Telefonpanel 1.0 1C-hez',
-    'SubHeaderModulePT1CCore' => 'Modul a számítógépes hívásvezérléshez',
+    'SubHeaderModulePT1CCore' => 'Modul a hívások kezelésére számítógépről',
     'module_pt1c_coreConnected' => 'Modul csatlakoztatva',
     'module_pt1c_coreDisconnected' => 'Modul letiltva',
     'module_pt1c_coreUpdateStatus' => 'Állapotfrissítés',
     'module_pt1c_core_WebinarLinkRussian' => 'Webinárium a modul beállításáról',
     'module_pt1c_core_Forum' => 'Fórum',
     'module_pt1c_core_LinksRussian' => 'Hasznos linkek (orosz)',
-    'f2b_Jail_module_p_t1_c_core' => 'Hibák a CTI-ügyfelek vagy 1C-panelek engedélyezése során',
-    'module_pt1c_core_WikiDocsRussian' => 'Wiki dokumentáció a modul konfigurálásáról',
-    'fw_modulept1ccoreDescription' => 'CTI CLIENT - Telefonos panel 1.0 az 1C számára',
+    'f2b_Jail_module_p_t1_c_core' => 'Hibák a CTI-kliensek vagy az 1C-panelek engedélyezése során',
+    'module_pt1c_core_WikiDocsRussian' => 'Wiki dokumentáció a modul beállításához',
+    'fw_modulept1ccoreDescription' => 'CTI CLIENT - Telefonpanel 1.0 1C-hez',
+    /**
+ * Copyright (C) MIKO LLC - All Rights Reserved
+ * Unauthorized copying of this file, via any medium is strictly prohibited
+ * Proprietary and confidential
+ * Written by Nikolay Beketov, 6 2018
+ *
+ */
+    'repModulePT1CCore' => 'Telefonpanel 1.0, 1C:Enterprise – %repesent%',
+    'mo_ModuleModulePT1CCore' => 'Telefonpanel 1.0 1C-hez',
 ];

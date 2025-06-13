7955315892158a893145dc988628cfd1bd1e22d5
SUMONCHAI WONGPITAK <sumonchai@gmail.com>
2025-06-13 02:02:36 +0000

    Translated using Weblate (Thai)
    Currently translated at 100.0% (13 of 13 strings)
    
    Translation: MIKOPBX/ModulePT1CCore
    Translate-URL: https://weblate.mikopbx.com/projects/mikopbx/modulept1ccore/th/

--- Messages/th.php
+++ Messages/th.php
@@ -9,4 +9,15 @@ return [
     'module_pt1c_core_Forum' => 'ฟอรั่ม',
     'module_pt1c_core_LinksRussian' => 'ลิงค์ที่มีประโยชน์ (รัสเซีย)',
     'fw_modulept1ccoreDescription' => 'CTI CLIENT - แผงโทรศัพท์ 1.0 สำหรับ 1C',
+    /**
+ * Copyright (C) MIKO LLC - All Rights Reserved
+ * Unauthorized copying of this file, via any medium is strictly prohibited
+ * Proprietary and confidential
+ * Written by Nikolay Beketov, 6 2018
+ *
+ */
+    'repModulePT1CCore' => 'แผงโทรศัพท์ 1.0 สำหรับ 1C: Enterprise - %repesent%',
+    'mo_ModuleModulePT1CCore' => 'แผงโทรศัพท์ 1.0 สำหรับ 1C: Enterprise',
+    'BreadcrumbModulePT1CCore' => 'แผงโทรศัพท์ 1.0 สำหรับ 1C: Enterprise',
+    'f2b_Jail_module_p_t1_c_core' => 'ข้อผิดพลาดในการตรวจสอบสิทธิ์ของลูกค้า CTI หรือแผง 1C',
 ];
